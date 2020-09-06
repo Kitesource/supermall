4550ba2c9204615eb16bfb88810048d7223f7f65
Kitesource <2257440643@qq.com>
2020-09-06 20:30:25 +0800

    初始化

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tab-bar-item" @click="itemClick">
+        <div v-if="!isActive"><slot name="item-icon"></slot></div>
+        <div v-else ><slot name="item-icon-active"></slot></div>
+        <div :style="activeStyle"><slot name="item-text"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabBarItem',
+    props: {
+        path: String,
+        activeColor: {
+            type:String,
+            defaul: '#d81e06 '
+        }
+    },
+    data() {
+        return{
+            // isActive:false
+        }
+    },
+    computed: {
+        isActive(){
+            // 如果要检索的字符串值没有出现，则该方法返回 -1。
+            return this.$route.path.indexOf(this.path) !== -1
+        },
+        activeStyle(){
+            return this.isActive ? {color:this.activeColor} : {}
+        }
+    },
+    methods: {
+        itemClick() {
+            // console.log('itemClick');
+            this.$router.push(this.path)
+        }
+    }
+}
+</script>
+
+<style  scoped>
+    .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-item img{
+      width: 24px;
+      height: 24px;
+      margin-top: 3px;
+      margin-bottom: 1px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//懒加载
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Shopcart = () => import('../views/shopcart/Shopcart')
+const Mine = () => import('../views/mine/Mine')
+
+//1.安装插件
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/category',
+        component: Category
+    },
+    {
+        path: '/shopcart',
+        component: Shopcart
+    },
+    {
+        path: '/mine',
+        component: Mine
+    }
+]
+
+//2.创建router
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
+//3.导出
+export default router
