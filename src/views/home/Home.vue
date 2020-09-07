8ed7007711737c0243d625c89e4c70ec01d4bca1
Kitesource <2257440643@qq.com>
2020-09-07 10:23:11 +0800

    基本搭建和首页开发

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,10 @@
 <template>
     <div id="home">
-        <nav-bar class="home-nav">
-            <div slot="center">购物街</div>
-        </nav-bar>
+        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banners="banners"/>
         <recommend-view :recommends="recommends"/>
         <feature-view/>
-        <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+        <tab-control :titles="['流行', '新款', '精选']" class="tab-control"/>
 
         <ul>
             <li>列表1</li>
@@ -19,113 +17,96 @@
             <li>列表8</li>
             <li>列表9</li>
             <li>列表10</li>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
+            <li>列表11</li>
+            <li>列表12</li>
+            <li>列表13</li>
+            <li>列表14</li>
+            <li>列表15</li>
+            <li>列表16</li>
+            <li>列表17</li>
+            <li>列表18</li>
+            <li>列表19</li>
+            <li>列表20</li>
+            <li>列表21</li>
+            <li>列表22</li>
+            <li>列表23</li>
+            <li>列表24</li>
+            <li>列表25</li>
+            <li>列表26</li>
+            <li>列表27</li>
+            <li>列表28</li>
+            <li>列表29</li>
+            <li>列表30</li>
+            <li>列表31</li>
+            <li>列表32</li>
+            <li>列表33</li>
+            <li>列表34</li>
+            <li>列表35</li>
+            <li>列表36</li>
         </ul>
-
     </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
 import FeatureView from './childComps/FeatureView'
 
-import NavBar from 'components/common/navbar/NavBar'
-import TabControl from 'components/content/tabControl/TabControl'
-
-import {getHomeMultidata, getHomeGoods} from 'network/home'
+import {getHomeMultidata, getHomeGoods } from 'network/home'
 
 export default {
     name: 'Home',
     components: {
+        NavBar,
         HomeSwiper,
         RecommendView,
         FeatureView,
-        NavBar,
-        TabControl,
+        TabControl
     },
     data() {
         return {
-         banners: [],
-         recommends: [],
-         goods: {
-             'pop': {page:0, list: []},
-             'new': {page:0, list: []},
-             'sell': {page:0, list: []}
-         }
+            banners: [],
+            recommends: [],
+            goods: {
+                'pop' :{page: 0, list: []},
+                'new' :{page: 0, list: []},
+                'sell' :{page: 0, list: []}
+            }
         }
     },
-     created() {
-      // 1.请求多个数据
-      this.getHomeMultidata()
-
-      // 2.请求商品数据
-      this.getHomeGoods('pop')
-      this.getHomeGoods('new')
-      this.getHomeGoods('sell')
+    created() {
+        //1.请求多个数据
+        this.getHomeMultidata();
+        //2.请求商品数量
+        this.getHomeGoods('pop');
+        this.getHomeGoods('new');
+        this.getHomeGoods('sell');
     },
     methods: {
         getHomeMultidata() {
-        getHomeMultidata().then(res => {
-          // this.result = res;
-          this.banners = res.data.banner.list;
-          this.recommends = res.data.recommend.list;
-        })
-      },
-      getHomeGoods(type) {
-        const page = this.goods[type].page + 1
-        getHomeGoods(type, page).then(res => {
-          this.goods[type].list.push(...res.data.list)
-          this.goods[type].page += 1
-
-          this.$refs.scroll.finishPullUp()
-        })
-      }
-    }   
+            getHomeMultidata().then(res => {
+                // console.log(res);
+                this.banners = res.data.banner.list;
+                this.recommends = res.data.recommend.list;
+            })
+        },
+        getHomeGoods(type) {
+            const page = this.goods[type].page + 1;
+            getHomeGoods(type,page).then(res => {
+                this.goods[type].list.push(...res.data.list)
+                this.goods[type].page += 1
+            })
+        }
+    }
 }
 </script>
 
 <style  scoped>
-    #home{
+
+    #home {
         padding-top: 44px;
     }
 
@@ -139,8 +120,9 @@ export default {
         z-index: 9;
     }
 
-    .tab-control {
+    .tab-control{
         position: sticky;
         top: 44px;
     }
+
 </style>
--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -1,33 +1,33 @@
 <template>
     <swiper>
-        <swiper-item v-for="item in banners">
-            <a :href="item.link">
-                <img :src="item.image" alt="">
-            </a>
-        </swiper-item>
-    </swiper>
+            <swiper-item v-for="item in banners">
+                <a :href="item.link">
+                    <img :src="item.image" alt="">
+                </a>
+            </swiper-item>
+        </swiper>
 </template>
 
 <script>
 import {Swiper, SwiperItem} from 'components/common/swiper'
 
-export default {
-    name:'HomeSwiper',
-    props:{
+  export default {
+    name: 'HomeSwiper',
+    props: {
         banners: {
-            type:Array,
+            type: Array,
             default() {
                 return []
             }
         }
     },
     components: {
-        SwiperItem,
-        Swiper
+        Swiper,
+        SwiperItem
     }
 }
 </script>
 
-<style  scoped>
-
+<style>
+  
 </style>
